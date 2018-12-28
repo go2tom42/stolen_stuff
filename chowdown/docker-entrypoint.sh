01ae0274d2f2d1440394a921a49ee324e77ab0b7
go2tom42 <tom42martin@gmail.com>
2018-12-28 03:07:09 -0500

    Update docker-entrypoint.sh

--- chowdown/docker-entrypoint.sh
+++ chowdown/docker-entrypoint.sh
@@ -5,8 +5,4 @@ if [ ! -f _config.yml ] && [ -n "$TARGET_REPO" ]; then
   git clone -b $TARGET_BRANCH --single-branch --depth 1 --recursive $TARGET_REPO /site
 fi
 
-if [ -f Gemfile ]; then
-  bundle install
-fi
-
 exec "$@"
