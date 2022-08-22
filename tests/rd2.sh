a88285d6c51b84f9a0437fe728e08cdd662d81fe
tom42 <tom42martin@gmail.com>
2022-08-21 23:46:49 -0400

    Update rd2.sh

--- tests/rd2.sh
+++ tests/rd2.sh
@@ -25,8 +25,8 @@ cd /bitnami/mediawiki
 
 sudo -Hu bitnami php /opt/bitnami/mediawiki/maintenance/importDump.php --conf ./LocalSettings.php /wikidump/reddwarffandomcom-20220813-wikidump/reddwarffandomcom-20220813-current.xml --username-prefix="" 
 sudo -Hu bitnami php /opt/bitnami/mediawiki/maintenance/importImages.php /wikidump/reddwarffandomcom-20220813-wikidump/images
+sudo -Hu bitnami php /opt/bitnami/mediawiki/maintenance/updateArticleCount.php --update
 sudo -Hu bitnami php /opt/bitnami/mediawiki/maintenance/rebuildall.php
-sudo -Hu bitnami php /opt/bitnami/mediawiki/maintenance/updateArticleCount.php
 sudo -Hu bitnami php /opt/bitnami/mediawiki/maintenance/update.php
 
 
